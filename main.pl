fc81c9dd66b159016f743669e3762bc1a7a7b7d0
Rhys Adams <rhys@therhys.co.uk>
2023-02-24 13:32:07 +0000

    Reformat code.

--- main.pl
+++ main.pl
@@ -13,143 +13,156 @@ use Route;
 use Attachment;
 use Rule;
 
+sub dnat {
+	my %args = @_;
+	my $dst = $args{dst};
+	my $source = $args{src};
+	my @ports = @{ $args{ports} };
+	
+	foreach my $port (@ports) {
+		$source->rule(
+			Rule->new (
+				chain => 'FORWARD',
+				stateful => 1,
+				proto => 'tcp',
+				dst => $dst,
+				dport => $port,
+				action => 'ACCEPT',
+			)
+		);
+		
+		$source->rule(
+			Rule->new (
+					table => 'nat',
+					chain => 'PREROUTING',
+					proto => 'tcp',
+					to_dst => $dst,
+					dport => $port,
+					action => 'DNAT',
+			)
+		);
+	}
+}
 
-my $lab = Lab->new(
+
+my $lab = Lab->new (
 	name => 'TestLab',
 	out_dir => 'res',
 	data_dir => 'data',
 );
 
 
-my $ext_www_lan = Lan->new('ExtWWW');
-my $dmz_lan = Lan->new('Dmz');
-my $staff_lan = Lan->new('Staff');
+my $ext_www_lan = Lan->new ('ExtWWW');
+my $dmz_lan = Lan->new ('Dmz');
+my $staff_lan = Lan->new ('Staff');
 
-my $r2 = Machine->new(
+my $r2 = Machine->new (
 	name => 'r2',
 	interfaces => [
-		Interface->new(
+		Interface->new (
 			eth => 0,
 			ip => '192.168.0.3/24',
 			mac => 'a8:20:66:2d:30:bf',
 		),
-		Interface->new(
+		Interface->new (
 			eth => 1,
 			ip => '10.0.0.1/20',
 			mac => 'a8:20:66:3e:42:cf',
 		),
 	],
 	routes => [
-		Route->new(
+		Route->new (
 			dst => 'default',
 			via => '192.168.0.1'
 		),
-		Route->new(
+		Route->new (
 			dst => '172.16.0.0/24',
 			via => '192.168.0.2'
 		),
 	],
 	attachments => [
-		Attachment->new(
+		Attachment->new (
 			lan => $dmz_lan,
 			eth => 0
 		),
-		Attachment->new(
+		Attachment->new (
 			lan => $staff_lan,
 			eth => 1
 		),
 	],
 	rules => [
-		Rule->new(
+		Rule->new (
 			policy => 'FORWARD DROP',
 		),
 	],
 );
 
-my $gw = Machine->new(
+my $gw = Machine->new (
 	name => 'gw',
 	interfaces => [
-		Interface->new(
+		Interface->new (
 			eth => 0,
 			ip => '80.64.157.254',
 		),
-		Interface->new(
+		Interface->new (
 			eth => 1,
 			ip => '192.168.0.1/24',
 		),
 	],
 	routes => [
-		Route->new(
+		Route->new (
 			dst => '172.16.0.0/24',
 			via => '192.168.0.2'
 		),
-		Route->new(
+		Route->new (
 			dst => '10.0.0.0/20',
 			via => '192.168.0.3'
 		),
 	],
 	attachments => [
-		Attachment->new(
+		Attachment->new (
 			lan => $ext_www_lan,
 			eth => 0
 		),
-		Attachment->new(
+		Attachment->new (
 			lan => $dmz_lan,
 			eth => 1
 		),
 	],
 	rules => [
-		Rule->new(
+		Rule->new (
 			policy => 'FORWARD DROP',
 		)
 	],
 );
 
-my $staff_1 = Machine->new(
+my $staff_1 = Machine->new (
 	name => 'Staff-1',
 	interfaces => [
-		Interface->new(
+		Interface->new (
 			eth => 0,
 			ip => '10.0.0.5/20',
 			mac => 'a8:30:67:3f:42:cf',
 		),
 	],
 	routes => [
-		Route->new(
+		Route->new (
 			dst => 'default',
 			via => '10.0.0.1'
 		),
 	],
 	attachments => [
-		Attachment->new(
+		Attachment->new (
 			lan => $staff_lan,
 			eth => 0,
 		),
 	],
 );
 
-for my $port (25, 587, 993) {
-	$gw->rule(
-		Rule->new(
-			chain => 'FORWARD',
-			stateful => 1,
-			proto => 'tcp',
-			dst => '172.16.0.6',
-			dport => $port,
-			action => 'ACCEPT',
-		)
-	);
-	
-	$gw->rule(
-		Rule->new(
-				table => 'nat',
-				chain => 'PREROUTING',
-				proto => 'tcp',
-				to_dst => '172.16.0.6',
-				dport => $port,
-				action => 'DNAT',
-		)
-	);
-}
+dnat (
+	src => $gw,
+	dst =>'172.16.0.6',
+	ports => [25, 587, 993]
+);
 
-$lab->dump($staff_1, $r2, $gw);
+$lab->dump($gw, $r2, $staff_1);
